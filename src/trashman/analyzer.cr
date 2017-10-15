5b46c5601b9a3164cfc96c9c2e0a937dbe08cf4e
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2017-10-15 18:14:03 +0200

    Copy memory stats to the analyzer so it becomes a snapshot

--- src/trashman/analyzer.cr
+++ src/trashman/analyzer.cr
@@ -1,28 +1,55 @@
+struct Trashman::AnalyzerRecord
+  @callstack : CallStack
+  @avg_lifetime : Time::Span
+  @allocations : UInt64
+  @deallocations : UInt64
+  @type : String
+
+  getter callstack, avg_lifetime, allocations, deallocations, type
+
+  def initialize(record)
+    @callstack = record.callstack
+    @avg_lifetime = record.calc_average_lifetime
+    @allocations = record.allocations
+    @deallocations = record.deallocations
+    @type = record.type_str
+  end
+end
+
 class Trashman::Analyzer
-  @sorter : Proc(BaseRecord, BaseRecord, Int32)
+  @sorter : Proc(AnalyzerRecord, AnalyzerRecord, Int32)
+  @records = [] of AnalyzerRecord
 
   property sorter
 
   def initialize
-    @sorter = ->(a : BaseRecord, b : BaseRecord) {
-      a.calc_average_lifetime <=> b.calc_average_lifetime
+    Statistics.guard=true
+    @sorter = ->(a : AnalyzerRecord, b : AnalyzerRecord) {
+      a.avg_lifetime <=> b.avg_lifetime
     }
+    process_records
+    Statistics.guard=false
   end
 
   def print_stats(io)
-    records = Statistics.records
-    records = records.sort &@sorter
+    records = @records.sort &@sorter
     records.each do |record|
       print_record io, record
     end
   end
 
   def print_record(io, record)
-    io.puts "#{record.type_str} -- Avg Lifetime: #{record.calc_average_lifetime}"
+    io.puts "#{record.type} -- Avg Lifetime: #{record.avg_lifetime}"
     io.puts "Allocations: #{record.allocations}, Deallocations: #{record.deallocations}"
     backtrace = record.callstack.printable_backtrace
     backtrace.each do |line|
       io.puts line
+    end
+  end
+
+  private def process_records
+    Statistics.records.each do |record|
+      @records << AnalyzerRecord.new record
     end
   end
 end
